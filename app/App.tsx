243922b201044a77d537cef69e799c60f3d07e9c
Young-Ho Kim <yghokim@younghokim.net>
2024-10-09 00:28:58 +0900

    Replaced google drive video emded to Youtube. Made title logo responsive to mobile screen.

--- app/App.tsx
+++ app/App.tsx
@@ -13,7 +13,7 @@ export function App() {
     <main className="container mx-auto px-4 sm:px-12 pt-6">
       <div>
         <div className="font-bold text-lg text-[#4592ec] mb-3">Research Work</div>
-        <img className="mb-2 h-[110px] ml-[-37px]" src={require("../public/images/logos/aacesstalk-logo.svg")}/> 
+        <img className="mb-2 h-[95px] ml-[-30px] md:h-[110px] md:ml-[-37px]" src={require("../public/images/logos/aacesstalk-logo.svg")}/> 
         <div className="font-light leading-8 sm:leading-[2.5rem] text-2xl sm:text-3xl">Fostering Communication between <strong>Minimally Verbal Autistic Children</strong> and <strong>Parents</strong> with Contextual Guidance and Card Recommendation</div>
       </div>
       
--- app/sections/Video.tsx
+++ app/sections/Video.tsx
@@ -2,6 +2,8 @@ import { Section } from "../components/Section"
 
 export const Video = () => {
     return <Section title="Demonstration Video">
-        <iframe src="https://drive.google.com/file/d/1mTnPr7D0XJdSBrYoH2pVJWRsAEJUCFcM/preview" allow="autoplay" className="aspect-video w-full"></iframe>
+        <div className="overflow-hidden rounded-xl border-4 border-slate-500/20">
+            <iframe src="https://www.youtube-nocookie.com/embed/5V_Z2lmj_XU?si=DfeClVWjJfVAHMKy" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen className="aspect-video w-full"/>
+        </div>
     </Section>
 }
