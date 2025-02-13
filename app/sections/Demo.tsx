4fa7d26e68775eec83dfa201924e40f47ced2d1e
Young-Ho Kim <yghokim@younghokim.net>
2025-02-13 20:35:12 +0900

    Added loop video.

--- app/sections/Demo.tsx
+++ app/sections/Demo.tsx
@@ -1,5 +1,7 @@
 export const Demo = () => {
-    return <div className="mt-10">
-        <img src={require("../../public/images/teaser.png")}/>
-    </div> 
+    return <div className="mt-10 rounded-xl overflow-hidden border border-4 border-slate-200 aspect-video">
+    <video className="teaser-loop" autoPlay loop muted playsInline>
+                    <source src={require('../../public/exploreself-demo-loop.mp4')} type="video/mp4" />
+                </video>
+</div> 
 }
